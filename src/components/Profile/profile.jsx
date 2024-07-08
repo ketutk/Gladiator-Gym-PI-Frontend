fec02b24407fbbda513dbe7308969d6a5bafd1da
ketutk <ketutkrisnakertajaya@gmail.com>
2024-07-08 15:35:47 +0700

    fixing profile

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -116,7 +116,7 @@ const Profile = ({ user, setShouldRefetch, token }) => {
                   }}
                 />
               </div>
-              <button type="submit" class="w-full bg-gray-900 text-white py-2 px-4 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600" disabled={isLoading}>
+              <button type="submit" class="w-full bg-gray-900 text-white py-2 px-4 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600" disabled={!name || isLoading}>
                 {name ? (isLoading ? "Mohon tunggu..." : "Perbarui") : "Memuat data..."}
               </button>
             </form>

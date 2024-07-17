7ba0a546c994e3161e9fa2d414c5201dc54ecf4e
ketutk <ketutkrisnakertajaya@gmail.com>
2024-07-17 20:19:51 +0700

    modify navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -154,19 +154,6 @@ const Navbar = ({ childComponent: ChildComponent }) => {
               </a>
             </li>
             <li>
-              <a href="/" class={`flex items-center p-2  rounded-lg text-white "bg-gray-700" : ""} hover:bg-gray-700 group`}>
-                <svg class="flex-shrink-0 w-6 h-6  transition duration-75 text-gray-400  group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="4 2 21 20">
-                  <path
-                    fill-rule="evenodd"
-                    d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-
-                <span class="flex-1 ms-3 whitespace-nowrap">Beranda</span>
-              </a>
-            </li>
-            <li>
               <a
                 class="flex items-center p-2 rounded-lg text-white hover:bg-gray-700 group cursor-pointer"
                 onClick={(e) => {
